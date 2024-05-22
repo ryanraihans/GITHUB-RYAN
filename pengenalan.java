388410727fcd73f92ca5b47c3299d91537ed6923
RYAN <ryanraihans@gmail.com>
2024-05-22 09:24:06 +0700

    menambahkan fungsi matematik sin

--- pengenalan.java
+++ pengenalan.java
@@ -1,5 +1,8 @@
 public class pengenalan {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        int x= 10;
+        System.out.println(Math.sin(x));
     }
 }
