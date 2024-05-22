20ff26eb84fb4b89384cdcd34ba9bc7944588189
RYAN <ryanraihans@gmail.com>
2024-05-22 09:38:19 +0700

    feat : mengganti nilai sin

--- pengenalan.java
+++ pengenalan.java
@@ -2,7 +2,8 @@ public class pengenalan {
     public static void main(String[] args) {
         System.out.println("Hello");
 
-        int x= 10;
+        int x= 90;
         System.out.println(Math.sin(x));
+        //mengganti nilai sin
     }
 }
